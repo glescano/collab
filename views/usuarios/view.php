9430677a923eac8fd582fc107221ed06b01aaa30
Alberto Palavecino <betopala@hotmail.com>
2018-04-05 16:24:38 -0300

    cambios en cuanto a traducción

--- views/usuarios/view.php
+++ views/usuarios/view.php
@@ -15,11 +15,11 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
+        <?= Html::a('Actualizar', ['updates', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('Borrar', ['delete', 'id' => $model->id], [
             'class' => 'btn btn-danger',
             'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
+                'confirm' => '¿Seguro que quiere eliminar este usuario?',
                 'method' => 'post',
             ],
         ]) ?>
